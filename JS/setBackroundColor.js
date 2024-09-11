05aeb7488be3987803ca19b422740f624c3890b3
emre <Emse0001@stud.kea.dk>
2024-09-11 08:55:41 +0200

    first commit

---
+++ JS/setBackroundColor.js
@@ -0,0 +1,46 @@
+console.log("Jeg er i set backround color")
+
+function setBackroundColor(){
+    let col = inp.value;
+    console.log(col);
+    bdy.style.backgroundColor = col;
+    inpColorValue.value = bdy.style.backgroundColor;
+    pbCol.style.color = col;
+}
+
+function useColorPicker(){
+    let col = inpColorPicker.value
+    console.log(col);
+    bdy.style.backgroundColor = col;
+    pbCol.style.color = col;
+    inpColorValue = bdy.style.backgroundColor;
+}
+
+let inpColorValue = document.querySelector(".inpColorValue");
+let inpColorPicker = document.getElementById("inpColorPicker");
+
+let inp = document.querySelector(".inpColor");
+console.log(inp)
+
+let pbCol = document.querySelector(".pbSetColor")
+console.log(pbCol)
+pbCol.textContent = "Tryk mig for at sætte color"
+
+const pTags = document.getElementsByTagName('p')
+console.log(pTags)
+
+let pTagarr = Array.from(pTags);
+console.log("pTagarr");
+console.log(pTagarr)
+pTagarr.forEach(p => p.textContent = "Din kurder det er foreach")
+
+let bdy = document.querySelector("body")
+console.log(bdy)
+
+pbCol.addEventListener('click', setBackroundColor);
+
+document.addEventListener('keyup', setBackroundColor);
+
+inpColorPicker.addEventListener("input", useColorPicker)
+
+
